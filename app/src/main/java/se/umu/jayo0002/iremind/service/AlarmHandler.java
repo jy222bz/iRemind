da0490b5db9754f41b3b1d79d2b1dc8e964825fa
jacobYousif <jy222bz@student.lnu.se>
2020-01-16 15:17:24 +0100

    * StatusBar Icon is set.

--- app/src/main/java/se/umu/jayo0002/iremind/service/AlarmHandler.java
+++ app/src/main/java/se/umu/jayo0002/iremind/service/AlarmHandler.java
@@ -4,7 +4,6 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import se.umu.jayo0002.iremind.Tags;
 import se.umu.jayo0002.iremind.models.Task;
@@ -31,11 +30,9 @@ public class AlarmHandler {
         intent.putExtra(Tags.BUNDLE, bundle);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, task.getId(), intent, PendingIntent.FLAG_ONE_SHOT);
         assert alarmManager != null;
-        if(Build.VERSION.SDK_INT < 23){
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP,task.getAlarmDateAndTime().getTimeInMillis(),pendingIntent);
-        } else{
-            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, task.getAlarmDateAndTime().getTimeInMillis(), pendingIntent);
-        }
+        AlarmManager.AlarmClockInfo alarmClockInfo=
+                new AlarmManager.AlarmClockInfo(task.getAlarmDateAndTime().getTimeInMillis(), pendingIntent);
+        alarmManager.setAlarmClock(alarmClockInfo, pendingIntent);
     }
 
 
